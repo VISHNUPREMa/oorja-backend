ce4f6a49c51187f69c891a40841098fb72f188a1
vishnu-prem-gh <vishnu@growhut.in>
2024-10-04 13:46:01 +0530

    database changedto atlas

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect("mongodb+srv://vishnu:h6MZk3PIOFETLFcS@oorja.p1ahd.mongodb.net/?retryWrites=true&w=majority&appName=oorja")
         console.log('Connected to MongoDB');
     } catch (err) {
         console.error('Error connecting to MongoDB:', err);
